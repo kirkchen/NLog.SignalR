9e12a91563a27d6aa76b59850fcab30c3762db78
Kirk Chen <rwk0119@yahoo.com.tw>
2014-04-01 13:54:25 +0800

    Initial NLog.SignalR Project

---
+++ Src/NLog.SignalR/SignalRTarget.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.SignalR;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.SignalR
+{
+    [Target("SignalR")]
+    public class SignalRTarget : TargetWithLayout
+    {
+        public IHubContext HubContext { get; set; }
+
+        public SignalRTarget()
+            : this(GlobalHost.ConnectionManager.GetHubContext<LogHub>())
+        {
+        }
+
+        public SignalRTarget(IHubContext hubContext)
+        {
+            this.HubContext = hubContext;
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string log = this.Layout.Render(logEvent);
+
+            this.SendMessageViaHub(log, logEvent);
+        }
+
+        private void SendMessageViaHub(string log, LogEventInfo logEvent)
+        {
+            this.HubContext.Clients.All.Log(log, logEvent);
+        }        
+    }
+}
